d38a301a775c39e93c63b1190fd5a425625ce376
Kevin Isom <kevin.isom@gmail.com>
2019-05-06 22:39:28 +1200

    Shopping Cart

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+<cart-icon /> {{cart.length}}
+  </div>
+</template>
+
+<script>
+  import CartIcon from './CartIcon.vue'
+  export default {
+    props: ['cart'],
+    components: {
+      CartIcon
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    font-size: .9rem;
+    text-align: right;
+    margin-top: 10px;
+    margin-right: 25px;
+  }
+</style>
