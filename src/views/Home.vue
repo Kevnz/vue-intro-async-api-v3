89daaf8dd49a3620e10c0110027557fd0180c29c
Kevin Isom <kevin.isom@gmail.com>
2019-05-10 11:36:02 +1200

    add links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,41 @@
 <template>
   <div class="home">
-    <ShoppingCart v-bind:cart="cart" />
-      <div id="app">
-        <img alt="Vue logo" src="@/assets/logo.png">
-        <Welcome msg="The Vue.js Shop"/>
-        <ProductList v-bind:products="products" v-on:add-to-cart="addToCart"></ProductList>
+    <ShoppingCart v-bind:cart="cart"/>
+    <div id="app">
+      <h1>
+        <img class="logo" alt="Vue logo" src="../assets/logo.png">The Vue.js Shop
+      </h1>
+      <div id="nav">
+        <router-link to="/">Home</router-link>|
+        <router-link to="/checkout">Checkout</router-link>|
+        <router-link to="/cart">Cart</router-link>|
+        <router-link to="/about">About</router-link>
       </div>
+      <ProductList v-bind:products="products" v-on:add-to-cart="addToCart"></ProductList>
+    </div>
   </div>
 </template>
 
 <script>
-
 // @ is an alias to /src
 import Welcome from "@/components/Welcome.vue";
 import ProductList from "@/components/ProductList.vue";
 import ShoppingCart from "@/components/ShoppingCart.vue";
 
-
 export default {
-  name: 'home',
+  name: "home",
   components: {
     ProductList,
     Welcome,
     ShoppingCart
   },
   data() {
-    console.info('Data in home')
+    console.info("Data in home");
     return {
       cart: window.cart || {
         total: 0,
         items: [],
-        cost: 0,
+        cost: 0
       },
       products: [
         {
@@ -47,7 +52,7 @@ export default {
           price: "960.00",
           category: "Games",
           description: "Licensed Rubber. Ameliorated demand-driven attitude.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}b`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}b`
         },
         {
           id: "8ded54bb-ed74-4a83-bf63-655915413da7",
@@ -55,7 +60,7 @@ export default {
           price: "831.00",
           category: "Baby",
           description: "Intelligent Cotton. Cloned next generation encoding.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}c`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}c`
         },
         {
           id: "58b4d888-b25d-4308-8811-44b0e9a58694",
@@ -63,7 +68,7 @@ export default {
           price: "796.00",
           category: "Sports",
           description: "Rustic Frozen. Polarised mobile emulation.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}d`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}d`
         },
         {
           id: "024f0048-3c50-4000-9cf8-e7bd87265bf5",
@@ -72,7 +77,7 @@ export default {
           category: "Electronics",
           description:
             "Awesome Metal. Ameliorated zero administration open system.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}e`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}e`
         },
         {
           id: "0f84caed-67d5-434c-b05a-13eb0b926598",
@@ -80,7 +85,7 @@ export default {
           price: "433.00",
           category: "Beauty",
           description: "Ergonomic Wooden. Ergonomic eco-centric archive.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}f`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}f`
         },
         {
           id: "092cd0cf-3f6b-4aba-979b-29d84f0caa52",
@@ -88,7 +93,7 @@ export default {
           price: "735.00",
           category: "Automotive",
           description: "Tasty Rubber. Exclusive clear-thinking extranet.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}g`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}g`
         },
         {
           id: "235b6497-f16b-41bc-b369-af702fcae7c1",
@@ -96,7 +101,7 @@ export default {
           price: "87.00",
           category: "Electronics",
           description: "Licensed Fresh. Profound composite leverage.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}h`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}h`
         },
         {
           id: "ea949e01-bd35-418d-9541-29c4c75e8250",
@@ -105,7 +110,7 @@ export default {
           category: "Shoes",
           description:
             "Intelligent Concrete. Universal content-based projection.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}i`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}i`
         },
         {
           id: "808d68c1-bbe8-4cfb-a869-9e0221b475e0",
@@ -113,7 +118,7 @@ export default {
           price: "965.00",
           category: "Baby",
           description: "Tasty Steel. Sharable bottom-line extranet.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}j`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}j`
         },
         {
           id: "61c4c7e0-d57e-4db4-b8c4-6157c572518f",
@@ -121,7 +126,7 @@ export default {
           price: "176.00",
           category: "Jewelery",
           description: "Handcrafted Metal. Open-source local initiative.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}k`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}k`
         },
         {
           id: "0168603b-8a10-4c50-99c7-fad9798879ff",
@@ -129,7 +134,7 @@ export default {
           price: "773.00",
           category: "Games",
           description: "Sleek Soft. Managed discrete process improvement.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}l`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}l`
         },
         {
           id: "9e82d3e0-9e70-4676-8adb-3d75a5a1e6f8",
@@ -137,7 +142,7 @@ export default {
           price: "848.00",
           category: "Jewelery",
           description: "Ergonomic Granite. Synchronised bifurcated time-frame.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}m`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}m`
         },
         {
           id: "de5c4a39-35cb-44b2-b5c2-6eab2dae8401",
@@ -145,7 +150,7 @@ export default {
           price: "430.00",
           category: "Outdoors",
           description: "Fantastic Cotton. Integrated directional workforce.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}n`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}n`
         },
         {
           id: "dd8e7010-9c0c-4938-9097-9726c9c2f865",
@@ -154,7 +159,7 @@ export default {
           category: "Tools",
           description:
             "Generic Concrete. Intuitive user-facing process improvement.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}o`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}o`
         },
         {
           id: "c0b2ffb9-b0f3-489a-9f08-b2de1aa575d5",
@@ -162,7 +167,7 @@ export default {
           price: "232.00",
           category: "Grocery",
           description: "Incredible Fresh. Adaptive incremental projection.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}p`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}p`
         },
         {
           id: "eb5a6638-6d90-427d-b741-50d721234d95",
@@ -171,7 +176,7 @@ export default {
           category: "Computers",
           description:
             "Gorgeous Concrete. Re-contextualized attitude-oriented system engine.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}q`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}q`
         },
         {
           id: "11b51b50-f355-4258-ae30-b43a81e00650",
@@ -179,7 +184,7 @@ export default {
           price: "997.00",
           category: "Outdoors",
           description: "Rustic Cotton. Focused impactful monitoring.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}r`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}r`
         },
         {
           id: "931bbf8b-ccf8-4fe9-99b4-2f2d17bf7cdc",
@@ -187,7 +192,7 @@ export default {
           price: "450.00",
           category: "Beauty",
           description: "Sleek Wooden. Multi-lateral asymmetric groupware.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}s`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}s`
         },
         {
           id: "37efef0b-88ee-4d4b-9964-be91001b969c",
@@ -195,31 +200,31 @@ export default {
           price: "257.00",
           category: "Outdoors",
           description: "Small Metal. Streamlined intangible neural-net.",
-                    image: `http://lorempixel.com/320/240?x=${Date.now()}t`
+          image: `http://lorempixel.com/320/240?x=${Date.now()}t`
         }
       ]
     };
   },
   methods: {
     addToCart(product) {
-      console.log('add to cart', this.cart.items)
-      if (this.cart.items.filter(i=> i.id === product.id).length === 0) {
-        this.cart.items.push({ ...product, quantity: 1 })
+      console.log("add to cart", this.cart.items);
+      if (this.cart.items.filter(i => i.id === product.id).length === 0) {
+        this.cart.items.push({ ...product, quantity: 1 });
       } else {
-        this.cart.items.filter(i => product.id === i.id)[0].quantity++
+        this.cart.items.filter(i => product.id === i.id)[0].quantity++;
       }
       const q = this.cart.items.reduce((quantity, entry) => {
-        return quantity + entry.quantity
-      }, 0)
+        return quantity + entry.quantity;
+      }, 0);
       const cost = this.cart.items.reduce((amount, entry) => {
-        return amount + (entry.quantity * entry.price)
-      }, 0)
-      this.cart.total = q
-      this.cart.cost = cost
+        return amount + entry.quantity * entry.price;
+      }, 0);
+      this.cart.total = q;
+      this.cart.cost = cost;
 
-      if (!window.cart) window.cart = {}
-      window.cart = this.cart
+      if (!window.cart) window.cart = {};
+      window.cart = this.cart;
     }
   }
-}
+};
 </script>
