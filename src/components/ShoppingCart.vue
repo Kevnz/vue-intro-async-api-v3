7356da22427ebdb58359fd5a6d21059c20f88da9
Kevin Isom <kevin.isom@gmail.com>
2019-05-10 11:34:46 +1200

    style, icon and router link

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,27 +1,35 @@
 <template>
   <div>
-    <cart-icon /> {{cart.total}}
+    <router-link to="/checkout">
+      <cart-icon/>
+      {{cart.total}}
+    </router-link>
   </div>
 </template>
 
 <script>
-  import CartIcon from './CartIcon.vue'
-  export default {
-    props: ['cart'],
-    components: {
-      CartIcon
-    },
-    methods: {
-
-    }
-  }
+import CartIcon from "./CartIcon.vue";
+export default {
+  props: ["cart"],
+  components: {
+    CartIcon
+  },
+  methods: {}
+};
 </script>
 
 <style scoped>
-  div {
-    font-size: .9rem;
-    text-align: right;
-    margin-top: 10px;
-    margin-right: 25px;
-  }
+div {
+  font-size: 0.9rem;
+  text-align: right;
+  margin-top: 10px;
+  margin-right: 15px;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 10px;
+}
+a {
+  color: #2c3e50;
+  text-decoration: none;
+}
 </style>
