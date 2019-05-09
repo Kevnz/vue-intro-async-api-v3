815c8736b5d53f6857ec46edac2242c1667bdf07
Kevin Isom <kevin.isom@gmail.com>
2019-05-10 11:34:58 +1200

    Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee, faChild, faCircle, faArchive, faCartPlus, faShoppingCart, faMinusSquare } from '@fortawesome/free-solid-svg-icons'
+import { faComment } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 
+library.add(
+  faCartPlus,
+  faCoffee,
+  faChild,
+  faCircle,
+  faArchive,
+  faComment,
+  faShoppingCart,
+  faMinusSquare,
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 Vue.config.productionTip = false
 
 new Vue({
