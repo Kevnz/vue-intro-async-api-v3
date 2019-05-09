1215a98d260b6a14586515549666e95a22be2f30
Kevin Isom <kevin.isom@gmail.com>
2019-05-10 11:35:14 +1200

    a few details

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <ShoppingCart v-bind:cart="cart"/>
+    <div id="app">
+      <h1>
+        <img class="logo" alt="Vue logo" src="../assets/logo.png">The Vue.js Shop
+      </h1>
+      <div id="nav">
+        <router-link to="/">Home</router-link>|
+        <router-link to="/checkout">Checkout</router-link>|
+        <router-link to="/cart">Cart</router-link>|
+        <router-link to="/about">About</router-link>
+      </div>
+      <div>This is an example application used to demonstrate using Vue.js</div>
+    </div>
   </div>
 </template>
+<script>
+import ShoppingCart from "@/components/ShoppingCart.vue";
+
+export default {
+  name: "about",
+  components: {
+    ShoppingCart
+  },
+  data() {
+    return {
+      products: window.cart || [],
+      cart: window.cart || []
+    };
+  }
+};
+</script>
